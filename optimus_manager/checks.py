ae2e8d61232b0d07b5a82ce9ce8ccd255e0c27f0
Hyper-KVM <hyperkvmx86@gmail.com>
2020-05-01 06:58:05 -0400

    fix OS detection (runit)

--- optimus_manager/checks.py
+++ optimus_manager/checks.py
@@ -46,6 +46,8 @@ def is_module_loaded(module_name):
     else:
         return True
 
+def detect_os():
+    return os.path.isdir("/run/runit/service")
 
 def detect_os():
     return os.path.isdir("/run/runit/service")
